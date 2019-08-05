406a4fbc01e387f3754a661dfe31959c4e4bd0eb
willianferreirax <50432534+willianferreirax@users.noreply.github.com>
2019-08-05 14:23:28 -0300

    adicionando ultima versão do banco 5/8

---
+++ freshr.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 05-Ago-2019 às 19:20
+-- Versão do servidor: 5.7.17
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `freshr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `evento`
+--
+
+CREATE TABLE `evento` (
+  `cod_evento` int(11) NOT NULL,
+  `nome_evento` varchar(30) DEFAULT NULL,
+  `banner_evento` varchar(30) DEFAULT NULL,
+  `data_inicio` date DEFAULT NULL,
+  `data_temino` date DEFAULT NULL,
+  `hora_inicio` time DEFAULT NULL,
+  `hora_termino` time DEFAULT NULL,
+  `endereco_evento` varchar(30) DEFAULT NULL,
+  `bairro_evento` varchar(30) DEFAULT NULL,
+  `cidade_evento` varchar(30) DEFAULT NULL,
+  `estado_evento` varchar(19) DEFAULT NULL,
+  `cep_evento` varchar(9) DEFAULT NULL,
+  `visibilidade_evento` tinyint(1) DEFAULT NULL,
+  `descricao_evento` varchar(150) DEFAULT NULL,
+  `preco_evento` float DEFAULT NULL,
+  `comp_qnt` int(11) DEFAULT NULL,
+  `interesse_qnt` varchar(30) DEFAULT NULL,
+  `CNPJ` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `faculdade`
+--
+
+CREATE TABLE `faculdade` (
+  `CNPJ` varchar(11) NOT NULL,
+  `login_inst` varchar(30) DEFAULT NULL,
+  `senha_inst` varchar(500) DEFAULT NULL,
+  `nome_inst` varchar(30) DEFAULT NULL,
+  `endereco_inst` varchar(80) DEFAULT NULL,
+  `bairro_inst` varchar(30) DEFAULT NULL,
+  `cidade_inst` varchar(30) DEFAULT NULL,
+  `estado_inst` varchar(19) DEFAULT NULL,
+  `cep_inst` varchar(9) DEFAULT NULL,
+  `email_inst` varchar(80) DEFAULT NULL,
+  `telefone_inst` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `interesses_evento`
+--
+
+CREATE TABLE `interesses_evento` (
+  `cod_interesse_evento` int(11) NOT NULL,
+  `interesseeve1` varchar(30) DEFAULT NULL,
+  `interesseeve2` varchar(30) DEFAULT NULL,
+  `interesseeve3` varchar(30) DEFAULT NULL,
+  `interesseeve4` varchar(30) DEFAULT NULL,
+  `interesseeve5` varchar(30) DEFAULT NULL,
+  `interesseeve6` varchar(30) DEFAULT NULL,
+  `interesseeve7` varchar(30) DEFAULT NULL,
+  `interesseeve8` varchar(30) DEFAULT NULL,
+  `interesseeve9` varchar(30) DEFAULT NULL,
+  `interesseeve10` varchar(30) DEFAULT NULL,
+  `interesseeve11` varchar(30) DEFAULT NULL,
+  `interesseeve12` varchar(30) DEFAULT NULL,
+  `interesseeve13` varchar(30) DEFAULT NULL,
+  `interesseeve14` varchar(30) DEFAULT NULL,
+  `interesseeve15` varchar(30) DEFAULT NULL,
+  `cod_evento` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `interesses_usuario`
+--
+
+CREATE TABLE `interesses_usuario` (
+  `cod_interesseusu` int(11) NOT NULL,
+  `interesseusu1` varchar(30) DEFAULT NULL,
+  `interesseusu2` varchar(30) DEFAULT NULL,
+  `interesseusu3` varchar(30) DEFAULT NULL,
+  `interesseusu4` varchar(30) DEFAULT NULL,
+  `interesseusu5` varchar(30) DEFAULT NULL,
+  `cod_usuario` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `cod_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(30) NOT NULL,
+  `sobrenome_usuario` varchar(30) NOT NULL,
+  `email_usuario` varchar(30) NOT NULL,
+  `senha_usuario` varchar(500) NOT NULL,
+  `login_usuario` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`cod_evento`),
+  ADD KEY `CNPJ` (`CNPJ`);
+
+--
+-- Indexes for table `faculdade`
+--
+ALTER TABLE `faculdade`
+  ADD PRIMARY KEY (`CNPJ`);
+
+--
+-- Indexes for table `interesses_evento`
+--
+ALTER TABLE `interesses_evento`
+  ADD PRIMARY KEY (`cod_interesse_evento`),
+  ADD KEY `cod_evento` (`cod_evento`);
+
+--
+-- Indexes for table `interesses_usuario`
+--
+ALTER TABLE `interesses_usuario`
+  ADD PRIMARY KEY (`cod_interesseusu`),
+  ADD KEY `cod_usuario` (`cod_usuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`cod_usuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `cod_evento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `interesses_evento`
+--
+ALTER TABLE `interesses_evento`
+  MODIFY `cod_interesse_evento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `interesses_usuario`
+--
+ALTER TABLE `interesses_usuario`
+  MODIFY `cod_interesseusu` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `cod_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
