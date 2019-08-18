4e0021c4c8f6bdbbac01ba767db31cca3a6f1986
iagoPB <50721035+iagoPB@users.noreply.github.com>
2019-08-18 13:40:17 -0300

    Update alterar_inst.php

--- alterar_inst.php
+++ alterar_inst.php
@@ -29,17 +29,194 @@ if (isset($_POST['login_inst'])) {
 
     </form>";
 }
-else{
+else if{
 	if (isset($_POST['senha_inst'])) {
 	
 
  	echo"<form name='alterar_f' method='POST' action='?validar=true'>
 
-        <h5>Escreva o novo login</h5><br>
+        <h5>Escreva a nova senha</h5><br>
 
-        <label><b>Login:</b></label>
+        <label><b>Senha:</b></label>
         <br>
-        <input type='login' name='login_inst' placeholder='Novo login'>
+        <input type='senha' name='senha_inst' placeholder='Nova senha'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+	
+	else if{
+	if (isset($_POST['nome_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva o novo nome</h5><br>
+
+        <label><b>Nome:</b></label>
+        <br>
+        <input type='text' name='nome_inst' placeholder='Novo nome'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+		
+	else if{
+		
+	if (isset($_POST['endereco_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva o novo endereço</h5><br>
+
+        <label><b>Endereço:</b></label>
+        <br>
+        <input type='text' name='endereço_inst' placeholder='Novo endereço'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+			
+		
+	
+	else if{
+		
+	if (isset($_POST['bairro_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva o novo bairro</h5><br>
+
+        <label><b>Bairro:</b></label>
+        <br>
+        <input type='text' name='bairro_inst' placeholder='Novo bairro'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+		
+		
+	else if{
+		
+	if (isset($_POST['cidade_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva a nova cidade</h5><br>
+
+        <label><b>Cidade:</b></label>
+        <br>
+        <input type='text' name='cidade_inst' placeholder='Nova cidade'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+			
+	else if{
+				
+	if (isset($_POST['cep_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva o novo CEP</h5><br>
+
+        <label><b>CEP:</b></label>
+        <br>
+        <input type='text' name='cep_inst' placeholder='Novo CEP'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+		
+	else if{
+				
+	if (isset($_POST['email_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva o novo CEP</h5><br>
+
+        <label><b>CEP:</b></label>
+        <br>
+        <input type='text' name='cep_inst' placeholder='Novo CEP'>
+        <br>
+        <br>
+    
+
+	    <input class='btn btn-info' type='submit' name='alterar' value='Alterar'>
+
+	    <br><br>
+
+	    <input class='btn btn-danger' type='reset' value='Limpar'>
+
+	    </form>";
+	}
+				
+       	else if{
+				
+	if (isset($_POST['telefone_inst'])) {
+	
+
+ 	echo"<form name='alterar_f' method='POST' action='?validar=true'>
+
+        <h5>Escreva novo telefone</h5><br>
+
+        <label><b>Telefone:</b></label>
+        <br>
+        <input type='text' name='telefone_inst' placeholder='Novo CEP'>
         <br>
         <br>
     
@@ -76,10 +253,38 @@ if(isset($_REQUEST["validar"]) && $_REQUEST["validar"] == true){
 	if(isset($_POST['login_inst'])){
 		$sql = "update faculdade set login_inst = '$login_inst'";
 	}
-	else{
+	else if{
 		if(isset($_POST['senha_inst'])){
 			$sql = "update faculdade set senha_inst = '$senha_inst'";
 		}
+	else if{
+		if(isset($_POST['nome_inst'])){
+			$sql = "update faculdade set nome_inst = '$nome_inst'";
+		}
+	else if{
+		if(isset($_POST['endereco_inst'])){
+			$sql = "update faculdade set endereco_inst = '$endereco_inst'";
+		}
+	else if{
+		if(isset($_POST['bairro_inst'])){
+			$sql = "update faculdade set bairro_inst = '$bairro_inst'";
+		}
+	else if{
+		if(isset($_POST['cidade_inst'])){
+			$sql = "update faculdade set cidade_inst = '$cidade_inst'";
+		}
+	else if{
+		if(isset($_POST['cep_inst'])){
+			$sql = "update faculdade set cep_inst = '$cep_inst'";
+		}
+	else if{
+		if(isset($_POST['email_inst'])){
+			$sql = "update faculdade set email_inst = '$email_inst'";
+		}
+	else if{
+		if(isset($_POST['telefone_inst"])){
+			$sql = "update faculdade set telefone_inst = '$telefone_inst'";
+	
 	}
 
 		$res = $conn->prepare($sql);
