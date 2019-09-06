aff67da88038f68edc4fee2f1c860c4b0b2fef64
Lucas Campanelli <48259776+lucascampanelli@users.noreply.github.com>
2019-09-06 18:48:18 -0300

    Aperfeiçoando o menu dropdown

--- index.php
+++ index.php
@@ -44,41 +44,33 @@
 			<label for='chec' class='backdiv'></label>
 		</nav>
 		<div class='dropdown'>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
-			<br>
 			<?php
 			session_start();
 			if(isset($_SESSION['usuario']))
 			{
-				echo $_SESSION['usuario'][0];
+				echo "<h1 class='imageuser'>".substr($_SESSION['usuario'][0], 0, strlen($_SESSION['usuario'][0]) - (strlen($_SESSION['usuario'][0])-1))."".substr($_SESSION['usuario'][4], 0, strlen($_SESSION['usuario'][4]) - (strlen($_SESSION['usuario'][4])-1))."</h1>";
 			}
 			if(isset($_SESSION['instituicao']))
 			{
-				echo $_SESSION['instituicao'][0];
+				echo "<h1 class='imageuser'>".substr($_SESSION['instituicao'][0], 0, strlen($_SESSION['instituicao'][0]) - (strlen($_SESSION['instituicao'][0])-2))."</h1>";
 			}
 			?>
 			<br>
-			<br>
 			<?php
 				if(isset($_SESSION['instituicao'][0])){
-					echo "<a href='painel_inst.php'>Minha Conta</a>";
+					echo "<a href='painel_inst.php' class='account'>Minha Conta</a>";
 				}
 				else{
-					echo "<a href='painel_usuario.php'>Minha Conta</a>";
+					echo "<a href='painel_usuario.php' class='account'>Minha Conta</a>";
 				}
 			?>
 			<br>
-			Configurações
+			<a href='painel_usuario.php' class='account'>Configurações</a>
 			<br>
-			Ajuda
+			<a href='painel_usuario.php' class='account'>Ajuda</a>
 			<br>
 			<br>
-			<a href='logout_script.php'>Sair</a>
+			<a href='logout_script.php' class='exit'>Sair</a>
 		</div>
 		<header class='cabecalhoindex' id='grid'>
 			<div class='menudiv'>
