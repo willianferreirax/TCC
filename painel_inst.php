76596b1b10003af0e512a36b7c7e0f02e14d29b1
Lucas Campanelli <48259776+lucascampanelli@users.noreply.github.com>
2019-10-05 21:50:26 -0300

    Corrigindo  o painel da instituição
    Implementando exibição de eventos criados e corrigindo responsividade.

--- painel_inst.php
+++ painel_inst.php
@@ -128,7 +128,7 @@
   				if(isset($_SESSION['instituicao'])){
   					echo "<div class='creatediv'><a href='eventinfo.php'><button class='cadastrarevent'>Criar evento</button></a>
   					</div></label>";
-  					echo "<div class='criaricon'><a href='cad_event.php'><i class='fas fa-plus'></i></a>
+  					echo "<div class='criaricon'><a href='eventinfo.php'><i class='fas fa-plus'></i></a>
   					</div></label>";
   				}
   				 ?>
