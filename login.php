df7a3a30f9812d973893719577de15c7630fe0aa
Lucas Campanelli <48259776+lucascampanelli@users.noreply.github.com>
2019-10-12 01:31:34 -0300

    Adicionando exibição de erro no login

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
   <link rel="stylesheet" href="css/auth.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="icon" href="images/icon.png">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width">
   <meta name="theme-color" content="#121212">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
@@ -38,9 +38,63 @@
       </a>
       <hr>
     </header>
+    <?php
 
+      if(isset($_REQUEST["validar"]) && $_REQUEST["validar"] == true){
+
+    		include 'connection.php';
+    		$conn = conexao();
+            session_start();
+            $login = $_POST['login'];
+            $senha = md5($_POST['senha']);
+
+
+    		$select = "SELECT cod_usuario,nome_usuario,email_usuario,senha_usuario, sobrenome_usuario from usuario where login_usuario = '$login' and senha_usuario = '$senha'";
+
+    		$res = $conn->prepare($select);//preparando query
+            $res->execute();//executando
+
+            $result = $res->fetchAll();//pegando todas as linhas da matriz
+
+            if (count($result) == 1 ){//contando quantas linhas tem, só deve haver 1 usuario com cada combinação de email e senha
+                $session=array();
+                foreach($result as $row){
+                  array_push($session,$row['nome_usuario']);//o array do SESSION terá na posição 0, o nome do usuario
+                  array_push($session,$row['email_usuario']);//o array do SESSION terá na posição 1, o email do usuario
+                  array_push($session,$row['senha_usuario']);//o array do SESSION terá na posição 2, a senha do usuario
+    			  			array_push($session,$row['cod_usuario']);//o array do SESSION terá na posição 3, o codigo do usuario
+    							array_push($session,$row['sobrenome_usuario']);//o array do SESSION terá na posição , o sobrenome do usuario
+                }
+                header('Location: select_interesse.php');
+                $_SESSION['usuario']=$session;//inserindo o array na variavel global _SESSION
+            }
+    		else{
+
+                $select = "SELECT CNPJ,nome_inst,email_inst,senha_inst from faculdade where login_inst = '$login' and senha_inst = '$senha'";
+                $res = $conn->prepare($select);//preparando query
+                $res->execute();//executando
+                $result = $res->fetchAll();
+
+                if (count($result) == 1 ){
+                    $session=array();
+                    foreach($result as $row){
+                        array_push($session,$row['nome_inst']);//o array do SESSION terá na posição 0, o nome da instituição
+                        array_push($session,$row['email_inst']);//o array do SESSION terá na posição 1, o email da instituição
+                        array_push($session,$row['senha_inst']);//o array do SESSION terá na posição 2, a senha da instituição
+    					array_push($session,$row['CNPJ']);//o array do SESSION terá na posição 3, o CNPJ da instituicao
+                    }
+                    header('Location: painel_inst.php');
+                    $_SESSION['instituicao']=$session;
+                }
+                else{
+                  echo "<div class='erro'>Usuário e/ou senha digitados estão incorretos.</div>";
+                }
+
+    		}
+    }
+    	?>
     <div class='containerform'>
-      <form action="login_script.php" method="post">
+      <form action="?validar=true" method="post">
         <br>
         <label>Login</label>
         <br>
