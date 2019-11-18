5a0467e6b9384e2cb3e619deb40ee7894b382101
Lucas Campanelli <48259776+lucascampanelli@users.noreply.github.com>
2019-11-17 21:52:27 -0300

    Adicionando ícone cabeçalho

--- alterar_inst.php
+++ alterar_inst.php
@@ -12,19 +12,24 @@ $cnpj = $_SESSION['instituicao'][3];
 <html lang="pt-br">
   <head>
     <title><?php echo $_SESSION['instituicao'][0];?> - FRESHR</title>
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
-    <meta charset="utf-8">
-    <meta name="description" content="Acesse o FRESHR. Uma visão de prosperidade para a sua carreira.">
-    <meta name="keywords" content="FRESHR, Eventos, Buscar, Profissão, Login">
-    <meta name="robots" content="Index, follow">
-    <meta name="author" content="Iago Pereira, Lucas Campanelli, Nicholas Campanelli, Renato Melo, Willian Ferreira">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/changeinfo.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="icon" href="images/icon.png">
-    <meta name="viewport" content="width=device-width">
-    <meta name="theme-color" content="#121212">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+		<!-- Required meta tags -->
+ 	 <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+ 	 <meta charset="utf-8">
+ 	 <meta name="description" content="Junte-se ao FRESHR. Uma visão de prosperidade para a sua carreira.">
+ 	 <meta name="keywords" content="FRESHR, Eventos, Buscar, Profissão, Página Inicial">
+ 	 <meta name="robots" content="Index, follow">
+ 	 <meta name="author" content="Iago Pereira, Lucas Campanelli, Nicholas Campanelli, Renato Melo, Willian Ferreira">
+ 	 <link rel="stylesheet" href="css/changeinfo.css">
+ 	 <link rel="stylesheet" href="css/style.css">
+ 	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 	 <script src="https://kit.fontawesome.com/337796870f.js"></script>
+ 	 <link rel="shortcut icon" href="images/icon.png">
+ 	 <meta name="viewport" content="width=device-width">
+ 	 <meta name="theme-color" content="#121212">
+ 	 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+ 	 <!-- Bootstrap CSS -->
+ 	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script type='text/javascript'>
   function mascara(formato, keypress, objeto){
     campo = eval(objeto);
@@ -60,20 +65,27 @@ $cnpj = $_SESSION['instituicao'][3];
         cep_inst.value = cep_inst.value + separador1;
     }
   }
+}
   </script>
   </head>
 <body>
 <center>
 
-  <header class='cabecalho'>
-      <a href="index.php" class='homea'>
-        <label for='logospace' class='logo'>
-          <h1 class='logospace'></h1>
-        </label>
-      </a>
-      <hr>
-    </header>
-	
+	<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<header class='cabecalho'>
+	 <a href="index.php" class='homea'>
+		 <label for='logospace' class='logo'>
+			 <h1 class='logospace'></h1>
+		 </label>
+	 </a>
+	 <hr>
+ </header>
+
 <?php
 
 if (isset($_POST['senha_inst'])) {
